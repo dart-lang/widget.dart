47f575a8e1a427460bc2cd9b708f6769cf5b1866
Kevin Moore <kevin@thinkpixellab.com>
2013-12-04 15:24:21 -0600

    running pub build from hop
    Progress on Issue #13

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -1,11 +1,11 @@
+import 'package:args/args.dart';
 import 'package:hop/hop.dart';
 import 'package:hop/hop_tasks.dart';
 
 import '../test/harness_console.dart' as test;
 
 void main(List<String> args) {
-  addTask('build', createProcessTask('dart', args: ['build.dart'],
-      description: "execute the project's build.dart file"));
+  addTask('build', _getBuildTask());
 
   addTask('test_dart2js',
       createDartCompilerTask(['test/browser_test_harness.dart']));
@@ -20,3 +20,20 @@ void main(List<String> args) {
 
   runHop(args);
 }
+
+const _MODE = 'mode';
+const _VERBOSE = 'verbose';
+
+Task _getBuildTask() =>
+    new Task((TaskContext ctx) {
+      var args = ['build', '--mode', ctx.arguments[_MODE]];
+
+      if(ctx.arguments[_VERBOSE]) {
+        args.add('--verbose');
+      }
+
+      return startProcess(ctx, 'pub', args);
+    }, config: (ArgParser parser) {
+      parser.addOption(_MODE, abbr: 'm', allowed: ['release', 'debug'], defaultsTo: 'debug');
+      parser.addFlag(_VERBOSE, abbr: 'v', defaultsTo: false);
+    });
