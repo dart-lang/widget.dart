27837d8ad2813ba84cf1ff9ec26c0ea2d8d52c7c
Kevin Moore <kevin@thinkpixellab.com>
2013-12-03 23:06:59 -0600

    more cleanup of hop_runner

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -7,20 +7,12 @@ void main(List<String> args) {
   addTask('build', createProcessTask('dart', args: ['build.dart'],
       description: "execute the project's build.dart file"));
 
-  final paths = ['web/out/index.html_bootstrap.dart'];
-  addTask('dart2js', createDartCompilerTask(paths,
-      minify: true, liveTypeAnalysis: true));
-
   addTask('test_dart2js',
       createDartCompilerTask(['test/browser_test_harness.dart']));
 
   addTask('test', createUnitTestTask(test.testCore));
 
-  //
-  // gh_pages
-  //
-  addTask('pages', (ctx) =>
-      branchForDir(ctx, 'master', 'build', 'gh-pages'));
+  addTask('pages', (ctx) => branchForDir(ctx, 'master', 'build', 'gh-pages'));
 
   addTask('update_js', createCopyJSTask('build',
       browserDart: true,
