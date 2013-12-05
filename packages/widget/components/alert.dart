611973a80c88d934171c30f25f301c99d2472680
Kevin Moore <kevin@thinkpixellab.com>
2013-12-05 14:31:02 -0600

    Contents of build from commit b18148c4

---
+++ packages/widget/components/alert.dart
@@ -0,0 +1,62 @@
+library widget.alert;
+
+import 'dart:async';
+import 'dart:html';
+import 'package:polymer/polymer.dart';
+import 'package:widget/effects.dart';
+import 'package:widget/widget.dart';
+
+/**
+ * [AlertWidget] follows the same convention as
+ * [its inspiration](http://getbootstrap.com/javascript/#alerts) in Bootstrap.
+ *
+ * Clicking on a nested element with the attribute `data-dismiss='alert'` will
+ * cause [AlertWidget] to close.
+ */
+@CustomTag('alert-widget')
+class AlertWidget extends PolymerElement implements ShowHideComponent {
+
+  AlertWidget.created(): super.created()  {
+    this.onClick.listen(_onClick);
+  }
+
+  bool get applyAuthorStyles => true;
+
+  bool _isShown = true;
+
+  bool get isShown => _isShown;
+
+  void set isShown(bool value) {
+    assert(value != null);
+    if(value != _isShown) {
+      _isShown = value;
+      final action = _isShown ? ShowHideAction.SHOW : ShowHideAction.HIDE;
+      ShowHide.begin(action, this, effect: new ScaleEffect());
+
+      ShowHideComponent.dispatchToggleEvent(this);
+    }
+  }
+
+  Stream<Event> get onToggle => ShowHideComponent.toggleEvent.forTarget(this);
+
+  void hide() {
+    isShown = false;
+  }
+
+  void show() {
+    isShown = true;
+  }
+
+  void toggle() {
+    isShown = !isShown;
+  }
+
+  void _onClick(MouseEvent event) {
+    if(!event.defaultPrevented) {
+      final Element target = event.target as Element;
+      if(target != null && target.dataset['dismiss'] == 'alert') {
+        hide();
+      }
+    }
+  }
+}
---
+++ packages/widget/components/collapse.dart
@@ -0,0 +1,95 @@
+library widget.collapse;
+
+import 'dart:async';
+import 'dart:html';
+import 'package:polymer/polymer.dart';
+import 'package:widget/effects.dart';
+import 'package:widget/widget.dart';
+
+/**
+ * [CollapseWidget] uses a content model similar to
+ * [collapse functionality](http://getbootstrap.com/javascript/#collapse)
+ * in Bootstrap.
+ *
+ * The header element for [CollapseWidget] is a child element with class
+ * `accordion-heading`.
+ *
+ * The rest of the children are rendered as content.
+ *
+ * [CollapseWidget] listens for `click` events and toggles visibility of content
+ * if the click target has attribute `data-toggle="collapse"`.
+ */
+@CustomTag('collapse-widget')
+class CollapseWidget extends PolymerElement implements ShowHideComponent {
+  static const String _COLLAPSE_DIV_SELECTOR = '.collapse-body-x';
+  static final ShowHideEffect _effect = new ShrinkEffect();
+
+  bool get applyAuthorStyles => true;
+
+  bool _isShown = false;
+
+  bool get isShown => _isShown;
+
+  bool _insertedCalled = false;
+
+  void set isShown(bool value) {
+    assert(value != null);
+    if(value != _isShown) {
+      _isShown = value;
+      _updateElements(!_insertedCalled);
+
+      ShowHideComponent.dispatchToggleEvent(this);
+    }
+  }
+
+  CollapseWidget.created() : super.created() {
+    this.onClick.listen(_onClick);
+  }
+
+  Stream<Event> get onToggle => ShowHideComponent.toggleEvent.forTarget(this);
+
+  void hide() {
+    isShown = false;
+  }
+
+  void show() {
+    isShown = true;
+  }
+
+  void toggle() {
+    isShown = !isShown;
+  }
+
+  @override
+  void enteredView() {
+    super.enteredView();
+
+    // TODO(jacobr): find a way to prevent animations upon calls to the isShown
+    // setter that occur from the intial web_ui template binding that do not
+    // require manually tracking _insertedCalled. If this.parent was null
+    // when the template binding occurred we would be fine.
+    _insertedCalled = true;
+    _updateElements(true);
+  }
+
+  void _onClick(MouseEvent e) {
+    if(!e.defaultPrevented) {
+      final clickElement = e.target as Element;
+
+      if(clickElement != null && clickElement.dataset['toggle'] == 'collapse') {
+        toggle();
+        e.preventDefault();
+      }
+    }
+  }
+
+  void _updateElements([bool skipAnimation = false]) {
+    var collapseDiv = getShadowRoot('collapse-widget')
+        .querySelector(_COLLAPSE_DIV_SELECTOR);
+    if(collapseDiv != null) {
+      var action = _isShown ? ShowHideAction.SHOW : ShowHideAction.HIDE;
+      var effect = skipAnimation ? null : _effect;
+      ShowHide.begin(action, collapseDiv, effect: effect);
+    }
+  }
+}
